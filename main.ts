63a745cf900bfc7f03eb2dac391dc319b63eb0ef
Matteo <de.notariis.matteo95@gmail.com>
2021-07-25 20:12:36 +0200

    First commit

---
+++ main.ts
@@ -0,0 +1,20 @@
+console.log("Hello Itachi");
+console.log("My first Node Project matafaka");
+
+let myVar: number = 3;
+myVar += 10;
+
+console.log("My var = ", myVar);
+
+interface MyInterface {
+    myNumber: number;
+    myString: string;
+}
+
+let myInterface: MyInterface = {
+    myNumber: 32,
+    myString: "The Gentlemen",
+};
+
+console.log("My number is ", myInterface.myNumber);
+console.log("My string is ", myInterface.myString);
